8c871ec5c4112641f84fb63f08422fe0332b41b4
mehithka <mehith.karacasia@gmail.com>
2025-02-05 15:20:29 +0500

    tuples

---
+++ a1.py
@@ -0,0 +1,18 @@
+tuplex= ('string', False, 1.23, 5)
+print(tuplex)
+tuplex = (4,2,5,3,6,1)
+print(tuplex)
+tuplex = tuplex +(9,)
+print(tuplex)
+
+tuple1=(50,40,12,150,20,40)
+print(tuple1.count(40))
+
+tuplex = (2,3,4,5,1,6,8,7,9,0)
+
+_slice = tuplex[3:5]
+
+print(_slice)
+
+_slice =  tuplex[:6]
+print(_slice)
